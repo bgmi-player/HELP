5c3bc41f5db6657674b4a1128dcf7c35c9989acc
Itsanupama <110755049+Itsanupama@users.noreply.github.com>
2024-05-02 15:13:37 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -206,7 +206,7 @@ Nᴀᴍᴇ - {}"""
 
 𝗠𝗲𝘀𝘀𝗮𝗴𝗲 <b>: {}</b>"""
 
-    CAPTION = """<b>📂Fɪʟᴇɴᴀᴍᴇ : {script.CAPTION}
+    CAPTION = """<b>📂Fɪʟᴇɴᴀᴍᴇ : {CUSTOM_FILE_CAPTION}
 
 <b> Size ⚙️: {file_size}</b>""" 
 
