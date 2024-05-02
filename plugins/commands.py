6a30bc8ba16f67683fea0e48af22a7938aca6517
Itsanupama <110755049+Itsanupama@users.noreply.github.com>
2024-05-02 18:39:31 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -593,7 +593,7 @@ async def start(client, message):
             logger.exception(e)
             f_caption=f_caption
     if f_caption is None:
-        f_caption = f"@Movies_Telugu_Top  {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith(''), files.file_name.split()))}"
+        f_caption = f"@Movies_Telugu_Top  {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), files.file_name.split()))}"
     if not await db.has_premium_access(message.from_user.id):
         if not await check_verification(client, message.from_user.id) and VERIFY == True:
             btn = [[
