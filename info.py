529b565e9906febcc0d0ebf49079ea31ede6d2b7
Itsanupama <110755049+Itsanupama@users.noreply.github.com>
2024-05-02 11:21:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -24,7 +24,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 1800))
-PICS = (environ.get('PICS', 'https://telegra.ph/file/ebad2eb29a3d2702e1531.jpg https://telegra.ph/file/5250452a1ab5364cfc4dc.jpg https://telegra.ph/file/68a707b00e8e88ed3c4a4.jpg https://telegra.ph/file/f44b3f7381cbf2e7708bb.jpg https://telegra.ph/file/4aaa6122c1fe3b71cf52c.jpg https://telegra.ph/file/486a9c2e80ed1729a345f.jpg https://telegra.ph/file/b165f958d038b903d43e1.jpg https://telegra.ph/file/989a744a60c061838444f.jpg https://telegra.ph/file/ff8f67820985179067982.jpg https://telegra.ph/file/3d8c714a3b0c04bfdec7e.jpg https://telegra.ph/file/fd0e1a19b0268c005edf8.jpg https://telegra.ph/file/8cfe0b5ceaa7b969448c4.jpg https://telegra.ph/file/5965fb86394de52b45e31.jpg https://telegra.ph/file/b826e4fabf1bf0fa72db3.jpg https://telegra.ph/file/baba127a57d74bc19e956.jpg')).split() #SAMPLE PIC
+PICS = (environ.get('PICS', 'https://graph.org/file/5fa7700881fc80a870700.jpg https://graph.org/file/cb890ea2dc2b33f2b2920.jpg https://graph.org/file/9917a86ae7723658f7a1a.jpg https://graph.org/file/09fd43a93bd88de8b8fa2.jpg https://graph.org/file/523e375c118eeadd7abfc.jpg https://graph.org/file/64fa4cc725baa9ca13c27.jpg https://graph.org/file/e077013200e1741ca2315.jpg https://graph.org/file/d91d8b72b11b486e21324.jpg https://graph.org/file/9dfab730138a7dba9630f.jpg https://graph.org/file/6a73d263e52c6b6bb9c65.jpg https://graph.org/file/9c99571314cce13b3b888.jpg')).split() #SAMPLE PIC
 NOR_IMG = environ.get("NOR_IMG", "https://te.legra.ph/file/a27dc8fe434e6b846b0f8.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://t.me/How_To_Open_shortLinks/19")
 SPELL_IMG = environ.get("SPELL_IMG", "https://te.legra.ph/file/15c1ad448dfe472a5cbb8.jpg")
